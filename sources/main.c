508de2c8b972b7547abd3df4c3c569bca209f181
imblackberry <blokhavictoria@gmail.com>
2020-03-03 15:51:18 +0200

    command initializing (start)

--- sources/main.c
+++ sources/main.c
@@ -1,4 +1,16 @@
-int main()
+#include "ft_ssl.h"
+
+int	main(int argc, char **argv)
 {
+	t_command command;
+
+	if (argc < 2)
+		return 0; //todo: error
+	if (init_command(argv, &command))
+		return -1; //todo: error
+	ft_printf("{%d}", command.flags);
+
+
+	(void)argc;
 	return 0;
 }
