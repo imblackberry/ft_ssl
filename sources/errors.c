4d11aa8a03eee0aa9eb2fb0697aabd88d8748a1d
imblackberry <blokhavictoria@gmail.com>
2020-04-15 13:15:48 +0300

    move newline in the error

--- sources/errors.c
+++ sources/errors.c
@@ -5,9 +5,9 @@ void	error_usage_msg(char *invalid_cmd)
 {
 	unsigned i;
 
-	ft_printf("ft_ssl: Error: \'%s\' is an invalid command.\n\n", invalid_cmd);
-	ft_printf("Standard commands:\n\n");
-	ft_printf("Message Digest commands:\n");
+	ft_printf("ft_ssl: Error: \'%s\' is an invalid command.\n", invalid_cmd);
+	ft_printf("\nStandard commands:\n");
+	ft_printf("\nMessage Digest commands:\n");
 	i = 0;
 	while (i < N_HASH)
 		ft_printf("%s\n", g_hash_name[i++]);
